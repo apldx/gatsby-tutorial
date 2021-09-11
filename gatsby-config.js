d20aa080bef84e3d26b3b3998f9bbf77592f3b49
Ajay Khanna <appliedstochastics@gmail.com>
2021-09-11 16:00:02 -0500

    05

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,5 +7,13 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+    "gatsby-plugin-mdx"
   ],
 };
